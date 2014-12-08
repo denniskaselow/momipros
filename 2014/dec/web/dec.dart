766bca9b554769196f149c85ba5f6a7a8d521c7d
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2014-12-08 22:53:11 +0100

    half a circle moving to sounds

--- 2014/dec/web/dec.dart
+++ 2014/dec/web/dec.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 import 'dart:web_audio';
 import 'dart:typed_data';
+import 'dart:math';
 
 const int fftSize = 512;
 const int frequencyBinCount = fftSize ~/ 2;
@@ -24,6 +25,9 @@ void main() {
     var byteTimeDomainDataCanvas = querySelector('#byteTimeDomainData') as CanvasElement;
     byteTimeDomainDataCanvas.width = 512;
     byteTimeDomainDataCanvas.height = 512;
+    var headCanvas = querySelector('#head') as CanvasElement;
+    headCanvas.width = 512;
+    headCanvas.height = 512;
 
     var floatFrequencyData = new Float32List(frequencyBinCount);
     var floatTimeDomainData = new Float32List(fftSize);
@@ -40,6 +44,32 @@ void main() {
       visualize(floatFrequencyDataCanvas, floatFrequencyData, 2, 256);
       visualize(byteTimeDomainDataCanvas, byteTimeDomainData, 1, 256);
       visualize(floatTimeDomainDataCanvas, floatTimeDomainData, 1, 1);
+
+      var valleyForE = byteFrequencyData.skip(14).take(4).reduce((a, b) => a+b);
+      var valleyForA = byteFrequencyData.skip(22).take(4).reduce((a, b) => a+b);
+      var diff = (valleyForE - valleyForA) / 4;
+      var sum = byteFrequencyData.reduce((a, b) => a+b);
+
+      headCanvas.context2D..fillStyle = 'white'
+                          ..fillRect(0, 0, 512, 512)
+                          ..save()
+                          ..fillStyle = 'black'
+                          ..save()
+                          ..translate(256, 256 - 2 * sum / fftSize)
+                          ..rotate(PI / 4 * diff / 256)
+                          ..beginPath()
+                          ..arc(0, 0, 64, PI, 0)
+                          ..fill()
+                          ..closePath()
+                          ..restore()
+                          ..save()
+                          ..translate(256, 256)
+                          ..beginPath()
+                          ..arc(0, 0, 64, 0, PI)
+                          ..fill()
+                          ..closePath()
+                          ..restore()
+                          ..restore();
 
       window.requestAnimationFrame(analyzeAndRender);
     }
