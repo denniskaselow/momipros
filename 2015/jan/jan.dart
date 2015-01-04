e6c389b9c7c76a87c75dd7b6c2464958c2f9621e
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2015-01-04 20:21:38 +0100

    debug version

---
+++ 2015/jan/jan.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+void main() {
+  var headingDiv = querySelector('#heading');
+  var speedDiv = querySelector('#speed');
+  var errorDiv = querySelector('#error');
+
+  try {
+    window.navigator.geolocation.watchPosition(enableHighAccuracy: true, timeout: new Duration(seconds: 10)).listen((data) {
+      headingDiv.innerHtml = 'Heading: ' + data.coords.heading;
+      speedDiv.innerHtml = 'Speed' + data.coords.speed;
+    })..onDone(() {
+      errorDiv.innerHtml = 'erledigt';
+    })..onError((e){
+      if (e is PositionError) {
+        errorDiv.innerHtml = (e as PositionError).message;
+      }
+    });
+  } catch (e) {
+    errorDiv.innerHtml = e;
+  }
+}
