c8c98c80e06217684be0953854d1023f9158776b
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2015-01-05 16:52:04 +0100

    update the existing map every time the position changes

--- 2015/jan/web/jan.dart
+++ 2015/jan/web/jan.dart
@@ -3,37 +3,28 @@ import 'dart:math';
 
 import 'package:google_maps/google_maps.dart';
 
+DivElement locationDiv = querySelector('#location');
+DivElement headingDiv = querySelector('#heading');
+DivElement speedDiv = querySelector('#speed');
+DivElement errorDiv = querySelector('#error');
+
+MapOptions options;
+GMap map;
 Polyline line;
 
 void main() {
-  var locationDiv = querySelector('#location');
-  var headingDiv = querySelector('#heading');
-  var speedDiv = querySelector('#speed');
-  var errorDiv = querySelector('#error');
+  map = new GMap(querySelector('#map'));
+  options = new MapOptions()..zoom = 15
+                            ..streetViewControl = false
+                            ..mapTypeId = MapTypeId.HYBRID;
+  var arrow = new GSymbol()..path = SymbolPath.FORWARD_OPEN_ARROW;
+  line = new Polyline(new PolylineOptions()..icons = [new IconSequence()..icon = arrow
+                                                                       ..offset = '100%']
+                                           ..map = map);
 
   window.navigator.geolocation.watchPosition(enableHighAccuracy: true).listen((data) {
     var coords = data.coords;
-    var options = new MapOptions()..center = new LatLng(coords.latitude, coords.longitude)
-                                  ..zoom = 15
-                                  ..streetViewControl = false
-                                  ..mapTypeId = MapTypeId.HYBRID;
-    locationDiv.innerHtml = 'Lat: ${coords.latitude} Long: ${coords.longitude}';
-    var map = new GMap(querySelector('#map'), options);
-    if (coords.heading == double.NAN || coords.heading == null || (coords.heading == 0.0 && coords.speed == 0.0)) {
-      headingDiv.innerHtml = 'Heading: UNKNOWN';
-      speedDiv.innerHtml = 'Speed: UNKNOWN';
-    } else {
-      headingDiv.innerHtml = 'Heading: ${coords.heading}';
-      speedDiv.innerHtml = 'Speed ${coords.speed} m/s';
-      var length = coords.speed * 0.0005;
-      var heading = coords.heading;
-      var arrow = new GSymbol()..path = SymbolPath.FORWARD_OPEN_ARROW;
-      line = new Polyline(new PolylineOptions()..icons = [new IconSequence()..icon = arrow
-                                                                            ..offset = '100%']
-                                               ..map = map
-                                               ..path = [new LatLng(coords.latitude + length * sin(heading - PI/2), coords.longitude + length * cos(heading - PI/2) ),
-                                                         new LatLng(coords.latitude + length * sin(heading + PI/2), coords.longitude + length * cos(heading + PI/2))]);
-    }
+    updateMap(coords.latitude, coords.longitude, coords.heading, coords.speed);
   }).onError((e) {
     if (e is PositionError) {
       errorDiv.innerHtml = (e as PositionError).message;
@@ -41,4 +32,21 @@ void main() {
       errorDiv.innerHtml = e.toString();
     }
   });
+}
+
+void updateMap(double latitude, double longitude, double heading, double speed) {
+  locationDiv.innerHtml = 'Lat: ${latitude} Long: ${longitude}';
+  options.center = new LatLng(latitude, longitude);
+  map.options = options;
+  if (heading == double.NAN || heading == null || (heading == 0.0 && speed == 0.0)) {
+    headingDiv.innerHtml = 'Heading: UNKNOWN';
+    speedDiv.innerHtml = 'Speed: UNKNOWN';
+  } else {
+    headingDiv.innerHtml = 'Heading: ${heading}';
+    speedDiv.innerHtml = 'Speed ${speed} m/s';
+    var length = speed * 0.0005;
+    heading = -heading * PI / 180;
+    line.path = [new LatLng(latitude + length * sin(heading - PI/2), longitude + length * cos(heading - PI/2) ),
+                 new LatLng(latitude + length * sin(heading + PI/2), longitude + length * cos(heading + PI/2))];
+  }
 }
